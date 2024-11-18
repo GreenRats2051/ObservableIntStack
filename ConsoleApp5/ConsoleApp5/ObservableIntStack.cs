77b39a81a2aeea3295ed16c673226d57ce715668
ITHUB\Student <qew>
2024-11-18 19:07:30 +0300

    .

---
+++ ConsoleApp5/ConsoleApp5/ObservableIntStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class ObservableIntStack
+    {
+        public event Action<int> ItemAdded;
+        public event Action<int> ItemRemoved;
+
+        private Stack<int> stack;
+
+        public ObservableIntStack(IEnumerable<int> initialValues)
+        {
+            stack = new Stack<int>(initialValues);
+        }
+
+        public void Subscribe(Action<int> subscriber)
+        {
+            if (subscriber == null) throw new ArgumentNullException(nameof(subscriber));
+            ItemAdded += subscriber;
+            ItemRemoved += subscriber;
+        }
+
+        public void Unsubscribe(Action<int> subscriber)
+        {
+            if (subscriber == null) throw new ArgumentNullException(nameof(subscriber));
+            ItemAdded -= subscriber;
+            ItemRemoved -= subscriber;
+        }
+
+        public void Push(int item)
+        {
+            stack.Push(item);
+            ItemAdded?.Invoke(item);
+        }
+
+        public int Pop()
+        {
+            int item = stack.Pop();
+            ItemRemoved?.Invoke(item);
+            return item;
+        }
+
+        public int Peek()
+        {
+            return stack.Peek();
+        }
+
+        public int Count => stack.Count;
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Subscriber1(int value) => Console.WriteLine($"Subscriber 1: Added/Removed {value}");
+            void Subscriber2(int value) => Console.WriteLine($"Subscriber 2: Added/Removed {value}");
+
+            var observableStack = new ObservableIntStack(new List<int> { 1, 2, 3 });
+
+            observableStack.Subscribe(Subscriber1);
+            observableStack.Subscribe(Subscriber2);
+
+            observableStack.Push(4);
+            observableStack.Push(5);
+            Console.WriteLine($"Current Count: {observableStack.Count}");
+
+            observableStack.Pop();
+            observableStack.Pop();
+            Console.WriteLine($"Current Count: {observableStack.Count}");
+        }
+    }
+}
